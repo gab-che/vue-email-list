0775f102e7ea55e4464a8298e80941bfdba3ee01
gab-che <gabriellachessa1@gmail.com>
2022-11-30 16:31:03 +0100

    fetches emails via api

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
+
         }
     },
 
     methods: {
-
+        fetchEmails(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(response){
+                console.log(response.data.response)
+            })
+        }
     },
+
+    mounted(){
+        this.fetchEmails();
+    }
 }).mount("#app");
