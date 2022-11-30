8e74c1c03bb32e1a8b5cbfed0fa8e2ada86166e6
gab-che <gabriellachessa1@gmail.com>
2022-11-30 17:21:38 +0100

    small improvement

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ createApp({
         },
 
         generateEmailsArray(){
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < this.counter; i++) {
                 this.fetchEmails();
             }
         }
